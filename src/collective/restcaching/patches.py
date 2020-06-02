764e800fab1b6c15c64771ce4f496d9b3c518ad4
mamico <mauro.amico@gmail.com>
2020-06-03 00:14:31 +0200

    vary

--- src/collective/restcaching/patches.py
+++ src/collective/restcaching/patches.py
@@ -6,9 +6,12 @@ def getRAMCacheKey(request, etag=None, lastModified=None):
     """Add Vary-ation headers in cache key
     """
     resourceKey = utils._old_getRAMCacheKey(request, etag=etag, lastModified=lastModified)
-    for header in request.response.getHeader('Vary', '').split(','):
-        value = request.getHeader(header)
-        if value:
-            resourceKey = '|{header}:{value}|{resourceKey}'.format(
-                header=header, value=value, resourceKey=resourceKey)
+    vary = request.response.getHeader('Vary')
+    if vary:
+        for header in vary.split(','):
+            header = header.strip()
+            value = request.getHeader(header)
+            if value:
+                resourceKey = '|{header}:{value}|{resourceKey}'.format(
+                    header=header, value=value, resourceKey=resourceKey)
     return resourceKey
