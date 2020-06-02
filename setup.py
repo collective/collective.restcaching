4f74cead461093b1b5856d40e85809f2102a6f71
mamico <mauro.amico@gmail.com>
2020-06-03 00:07:18 +0200

    purgebyid

--- setup.py
+++ setup.py
@@ -25,8 +25,8 @@ setup(
         "Framework :: Plone :: 5.2",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Operating System :: OS Independent",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
     ],
@@ -54,6 +54,7 @@ setup(
         'plone.api>=1.8.4',
         'plone.restapi',
         'plone.app.dexterity',
+        'collective.purgebyid',
     ],
     extras_require={
         'test': [
